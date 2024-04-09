0a418262d05dbf2f23e44771d4101b75eb6171e5
batalova-kira <128981298+batalova-kira@users.noreply.github.com>
2024-04-09 09:47:42 +0000

    Deploying to gh-pages from @ batalova-kira/learn-lingo-react@6836659fdc188e15728ba8022020ad3e21219b27 🚀

---
+++ assets/Favorites-Dqolk8oy.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-IjQGdO57.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/Teachers-CkeQHpOy.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CIjQFTrl.js";const s=()=>e.jsx("div",{children:"Teachers"});export{s as default};
