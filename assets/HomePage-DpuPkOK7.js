417bdd39e5b086d81f873134de8184e8fdf5ec70
batalova-kira <128981298+batalova-kira@users.noreply.github.com>
2024-04-09 21:26:48 +0000

    Deploying to gh-pages from @ batalova-kira/learn-lingo-react@5ca61ea91040abbd182821377a8acb2c7f7ea89e 🚀

---
+++ assets/HomePage-DpuPkOK7.js
@@ -0,0 +1,67 @@
+import{u as o,j as t,r as s,g as c,a as l,o as d,b as p}from"./index-D2b96rGN.js";const g=o.div`
+    display: flex;
+    gap: 24px;
+`,h=o.div`
+    padding: 98px 108px 98px 64px;
+    max-width: 720px;
+    margin-left: -64px;
+
+    border-radius: 30px;
+    background: ${e=>e.theme.colors.secondBackground};
+`,x=o.h1`
+    max-width: 548px;
+    margin-bottom: 32px;
+
+    font-family: var(--font-family);
+    font-weight: 500;
+    font-size: 48px;
+    line-height: 1.17;
+    letter-spacing: -0.02em;
+
+    color: ${e=>e.theme.colors.black};
+`,u=o.span`
+    font-style: italic;
+    font-weight: 400;
+    border-radius: 8px;
+    background: ${e=>e.theme.colors.heroBackground};
+`,m=o.div`
+    max-width: 471px;
+    align-items: start;
+    margin-bottom: 64px;
+`,f=o.p`
+    width: 100%;
+    font-family: var(--font-family);
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 137%;
+    letter-spacing: -0.02em;
+    color: ${e=>e.theme.colors.black};
+`,b=o.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 16px 88px;
+
+    font-family: var(--font-family);
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 1.56;
+    color: ${e=>e.theme.colors.black};
+
+    border-radius: 12px;
+    color: ${e=>e.theme.colors.black};
+    background: ${e=>e.theme.colors.pink};
+
+    &:hover {
+        cursor: pointer;
+        background: ${e=>e.theme.colors.darkPink};
+    }
+`,y=o.div`
+    position: relative;
+    border-radius: 30px;
+    max-width: 568px;
+    max-height: 530px;
+    margin-right: -64px;
+
+    background: ${e=>e.theme.colors.heroBackground};
+`,k="/learn-lingo-react/assets/girl-jRuYr3yu.svg",j=()=>t.jsx(t.Fragment,{children:t.jsxs(g,{children:[t.jsxs(h,{children:[t.jsxs(x,{children:["Unlock your potential with the best"," ",t.jsx(u,{children:"language"})," tutors"]}),t.jsx(m,{children:t.jsx(f,{children:"Embark on an Exciting Language Journey with Expert Language Tutors: Elevate your language proficiency to new heights by connecting with highly qualified and experienced tutors."})}),t.jsx(b,{children:"Get started"})]}),t.jsx(y,{children:t.jsx("img",{src:k,alt:"happy girl with laptop"})})]})}),v=()=>(s.useEffect(()=>{const e=c(p),r=l(e),n=d(r,a=>{const i=a.val();console.log("Updated data:",i)});return()=>{n()}},[]),t.jsx(t.Fragment,{children:t.jsx(j,{})}));export{v as default};
---
+++ assets/Teachers-BJJCSugJ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D2b96rGN.js";const s=()=>e.jsx("div",{children:"Teachers"});export{s as default};
