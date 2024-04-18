caf3a1c9e2377a58b4fdebf53a6d925823b6d3f0
batalova-kira <128981298+batalova-kira@users.noreply.github.com>
2024-04-18 19:33:46 +0000

    Deploying to gh-pages from @ batalova-kira/learn-lingo-react@2436913bbf673afbca7f04182a603fe23f7790e7 🚀

---
+++ assets/Teachers-uY3GWf_8.js
@@ -0,0 +1,98 @@
+import{u as r,G as g,j as t,r as h,g as f,a as m,o as u,b as v}from"./index-CH1r2Xqn.js";const C=r.div`
+    display: flex;
+    /* gap: 48px; */
+
+    max-width: 1184px;
+    padding: 24px;
+
+    border-radius: 24px;
+    background-color: ${e=>e.theme.colors.background};
+`,w=r.div`
+    display: block;
+    position: relative;
+    margin-right: 48px;
+`,j=r.img`
+    width: 144px;
+    height: 120px;
+    padding: 9px;
+    border-radius: 100px;
+    border: 3px solid ${e=>e.theme.colors.pink};
+    background-color: ${e=>e.theme.colors.background};
+`,b=r.img`
+    top: 20px;
+    left: 87px;
+    position: absolute;
+`,k="data:image/svg+xml,%3csvg%20width='12'%20height='12'%20viewBox='0%200%2012%2012'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3ccircle%20cx='6'%20cy='6'%20r='6'%20fill='white'/%3e%3ccircle%20cx='6'%20cy='6'%20r='4'%20fill='%2338CD3E'/%3e%3c/svg%3e";function y(e){return g({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"},child:[]}]})(e)}function L(e){return g({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z"},child:[]}]})(e)}const H=r.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-bottom: 8px;
+`,$=r.div`
+    /* margin-right: auto; */
+`,T=r.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`,z=r.p`
+    margin-bottom: 8px;
+    font-family: var(--font-family);
+    font-weight: var(--font-weight-medium);
+    font-size: 16px;
+    line-height: 150%;
+    color: ${e=>e.theme.colors.grey};
+`,_=r.p`
+    font-family: var(--font-family);
+    font-weight: var(--font-weight-medium);
+    font-size: 24px;
+    line-height: 100%;
+    color: ${e=>e.theme.colors.black};
+    margin-bottom: 32px;
+`,B=r.div`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    /* justify-content: flex-end; */
+`,i=r.p`
+    margin-right: 16px;
+    font-family: var(--font-family);
+    font-weight: var(--font-weight-medium);
+    font-size: 16px;
+    line-height: 1.5;
+    color: ${e=>e.theme.colors.black};
+`,p=r.img`
+    padding-top: 4px;
+    margin-right: 4px;
+`,d=r.span`
+    margin-right: 16px;
+    color: ${e=>e.theme.colors.spanGrey};
+`,F=r.span`
+    margin-right: 48px;
+    color: ${e=>e.theme.colors.green};
+`,I=r.button`
+    border: none;
+    background: none;
+`,M=r(y)`
+    font-size: 22px;
+    color: ${e=>e.theme.colors.black};
+    cursor: pointer;
+    background: transparent;
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        color: #3470ff;
+    }
+`;r(L)`
+    font-size: 18px;
+    color: ${e=>e.theme.colors.black};
+    cursor: pointer;
+
+    transition: all 0.3s ease-in-out;
+
+    &:hover {
+        color: #3470ff;
+    }
+`;const W="data:image/svg+xml,%3csvg%20width='16'%20height='16'%20viewBox='0%200%2016%2016'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cpath%20d='M14.6667%204.13333C14.6667%203.3866%2014.6667%203.01323%2014.5213%202.72801C14.3935%202.47713%2014.1895%202.27316%2013.9387%202.14532C13.6534%202%2013.2801%202%2012.5333%202H12.2667C10.7732%202%2010.0265%202%209.45603%202.29065C8.95426%202.54631%208.54631%202.95426%208.29065%203.45603C8%204.02646%208%204.77319%208%206.26667V14L8.0667%2013.8999C8.5298%2013.2053%208.76135%2012.858%209.06727%2012.6065C9.33809%2012.3839%209.65016%2012.2169%209.9856%2012.1151C10.3645%2012%2010.7819%2012%2011.6168%2012H12.5333C13.2801%2012%2013.6534%2012%2013.9387%2011.8547C14.1895%2011.7268%2014.3935%2011.5229%2014.5213%2011.272C14.6667%2010.9868%2014.6667%2010.6134%2014.6667%209.86667V4.13333Z'%20stroke='black'%20stroke-width='1.5'%20stroke-linecap='round'%20stroke-linejoin='round'%20/%3e%3cpath%20d='M1.33325%204.13333C1.33325%203.3866%201.33325%203.01323%201.47858%202.72801C1.60641%202.47713%201.81038%202.27316%202.06126%202.14532C2.34648%202%202.71985%202%203.46659%202H3.73325C5.22673%202%205.97346%202%206.54389%202.29065C7.04566%202.54631%207.45361%202.95426%207.70927%203.45603C7.99992%204.02646%207.99992%204.77319%207.99992%206.26667V14L7.93322%2013.8999C7.47012%2013.2053%207.23857%2012.858%206.93265%2012.6065C6.66182%2012.3839%206.34976%2012.2169%206.01432%2012.1151C5.63542%2012%205.21799%2012%204.38313%2012H3.46659C2.71985%2012%202.34648%2012%202.06126%2011.8547C1.81038%2011.7268%201.60641%2011.5229%201.47858%2011.272C1.33325%2010.9868%201.33325%2010.6134%201.33325%209.86667V4.13333Z'%20stroke='black'%20stroke-width='1.5'%20stroke-linecap='round'%20stroke-linejoin='round'%20/%3e%3c/svg%3e",A="data:image/svg+xml,%3csvg%20width='16'%20height='16'%20viewBox='0%200%2016%2016'%20fill='none'%20xmlns='http://www.w3.org/2000/svg'%3e%3cg%20clip-path='url(%23clip0_44_287)'%3e%3cpath%20d='M6.20021%204.69703L8%201.28579L9.79979%204.69703C9.95896%204.9987%2010.2491%205.20947%2010.5851%205.26762L14.3856%205.92519L11.6975%208.69103C11.4598%208.93563%2011.3489%209.27666%2011.3975%209.61427L11.9465%2013.4319L8.48537%2011.7301C8.17929%2011.5795%207.82071%2011.5795%207.51463%2011.7301L4.05348%2013.4319L4.6025%209.61427C4.65105%209.27666%204.54024%208.93563%204.30252%208.69103L1.6144%205.92519L5.41486%205.26762C5.75095%205.20947%206.04104%204.9987%206.20021%204.69703Z'%20fill='%23FFC531'%20stroke='%23FFC531'%20stroke-width='1.2'/%3e%3c/g%3e%3cdefs%3e%3cclipPath%20id='clip0_44_287'%3e%3crect%20width='16'%20height='16'%20fill='white'/%3e%3c/clipPath%3e%3c/defs%3e%3c/svg%3e",V=({item:e})=>{const{name:o,surname:s,lessons_done:n,rating:a,price_per_hour:l}=e;return t.jsxs(T,{children:[t.jsxs(H,{children:[t.jsx($,{children:t.jsx(z,{children:"Languages"})}),t.jsxs(B,{children:[t.jsx(p,{src:W,alt:"book"}),t.jsx(i,{children:"Lessons online"}),t.jsx(d,{children:"|"}),t.jsxs(i,{children:["Lessons done: ",n]}),t.jsx(d,{children:"|"}),t.jsx(p,{src:A,alt:"star rating"}),t.jsxs(i,{children:["Rating: ",a]}),t.jsx(d,{children:"|"}),t.jsxs(i,{children:["Price / 1 hour:"," ",t.jsxs(F,{children:[l,"$"]})]}),t.jsx(I,{type:"button",children:t.jsx(M,{})})]})]}),t.jsxs(_,{children:[o," ",s]}),t.jsx("div",{children:"Description"})]})},D=({item:e})=>{const{avatar_url:o}=e;return t.jsxs(C,{children:[t.jsxs(w,{children:[t.jsx(j,{src:o,alt:"teacher"}),t.jsx(b,{src:k,alt:"online status"})]}),t.jsx(V,{item:e})]})},E=r.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+`,S=()=>{const[e,o]=h.useState([]);return h.useEffect(()=>{const s=f(v),n=m(s),a=u(n,l=>{const c=l.val();if(console.log("Updated data:",c),c&&typeof c=="object"){const x=Object.values(c);o(x)}});return()=>{a()}},[]),t.jsx(t.Fragment,{children:t.jsx(E,{children:e.map((s,n)=>t.jsx("li",{children:t.jsx(D,{item:s})},n))})})};export{S as default};
