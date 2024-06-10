a24b6e7e3b15ca78a6b4534c57c1426a8d69f4f8
batalova-kira <batalovakira@gmail.com>
2024-06-10 22:03:50 +0300

    Updates

---
+++ assets/HomePage-BCiPvWQr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-B9xOx2lJ.js";import{H as e}from"./Hero-D4evI4Jy.js";const s=()=>r.jsx(r.Fragment,{children:r.jsx(e,{})});export{s as default};
--- assets/HomePage-kRZ15YFI.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-D_M1vB6d.js";import{H as e}from"./Hero-BB1Tfngk.js";const s=()=>r.jsx(r.Fragment,{children:r.jsx(e,{})});export{s as default};
