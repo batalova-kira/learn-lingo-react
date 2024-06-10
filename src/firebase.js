73e37febdda61d4aca5173ab06b9675aecfe3da0
batalova-kira <batalovakira@gmail.com>
2024-06-10 16:38:27 +0300

    attempt 7 deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import firebase from "firebase/compat/app";
+import "firebase/auth";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
@@ -9,15 +11,25 @@ const STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
 const MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
 const APP_ID = import.meta.env.VITE_APP_ID;
 
-const firebaseConfig = {
+// const firebaseConfig = {
+//     apiKey: API_KEY,
+//     authDomain: AUTH_DOMAIN,
+//     projectId: PROJECT_ID,
+//     storageBucket: STORAGE_BUCKET,
+//     messagingSenderId: MESSAGING_SENDER_ID,
+//     appId: APP_ID,
+// };
+
+const app = firebase.initializeApp({
     apiKey: API_KEY,
     authDomain: AUTH_DOMAIN,
     projectId: PROJECT_ID,
     storageBucket: STORAGE_BUCKET,
     messagingSenderId: MESSAGING_SENDER_ID,
     appId: APP_ID,
-};
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
+});
+// export const auth = app.auth;
+export default app;
+// export default const  app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getDatabase();
